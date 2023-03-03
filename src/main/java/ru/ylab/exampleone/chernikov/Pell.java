533212ab87d2deed68f160dcfb84394c8d089d20
OvercomingJunk <overcomingjunk@yandex.ru>
2023-03-03 23:50:51 +0300

    Solved "Stars" and "Pell" tasks

---
+++ src/main/java/ru/ylab/exampleone/chernikov/Pell.java
@@ -0,0 +1,29 @@
+package ru.ylab.exampleone.chernikov;
+
+import java.util.Scanner;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 03.03.2023
+ */
+public class Pell {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            if (n == 0) {
+                System.out.println(0);
+            } else {
+                int[] numbers = new int[n + 1];
+                numbers[0] = 0;
+                numbers[1] = 1;
+                for (int i = 2; i < numbers.length; i++) {
+                    int a = numbers[i - 2];
+                    int b = numbers[i - 1];
+                    numbers[i] = b * 2 + a;
+                }
+                System.out.println(numbers[n]);
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/ylab/exampleone/chernikov/Stars.java
@@ -0,0 +1,24 @@
+package ru.ylab.exampleone.chernikov;
+
+import java.util.Scanner;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 03.03.2023
+ */
+public class Stars {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            String template = scanner.next();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    System.out.print(template);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
