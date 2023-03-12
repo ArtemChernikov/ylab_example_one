f7661420589cb3be539c1158a974f8582b3c375e
OvercomingJunk <overcomingjunk@yandex.ru>
2023-03-12 19:44:14 +0300

    Solved "SnilsValidator" task

---
+++ src/main/java/ru/ylab/exampleone/chernikov/tasktwo/snils_validator/SnilsValidatorImpl.java
@@ -0,0 +1,39 @@
+package ru.ylab.exampleone.chernikov.tasktwo.snils_validator;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 12.03.2023
+ */
+public class SnilsValidatorImpl implements SnilsValidator {
+    @Override
+    public boolean validate(String snils) {
+        boolean valid = true;
+        if (snils.length() == 0) {
+            valid = false;
+        } else if (!snils.matches("^[0-9]*$")) {
+            valid = false;
+        } else if (snils.length() != SNILS_LENGTH) {
+            valid = false;
+        } else {
+            int sum = 0;
+            for (int i = 0; i < LENGTH_OF_SNILS_NUMBER; i++) {
+                sum += Character.digit(snils.charAt(i), 10) * (LENGTH_OF_SNILS_NUMBER - i);
+            }
+            int checkControlNumber = 0;
+            if (sum < 100) {
+                checkControlNumber = sum;
+            } else if (sum > 100) {
+                checkControlNumber = sum % 101;
+                if (checkControlNumber == 100) {
+                    checkControlNumber = 0;
+                }
+            }
+            int realControlNumber = Integer.parseInt(snils.substring(LENGTH_OF_SNILS_NUMBER));
+            if (checkControlNumber != realControlNumber) {
+                valid = false;
+            }
+        }
+        return valid;
+    }
+}
---
+++ src/main/java/ru/ylab/exampleone/chernikov/tasktwo/snils_validator/SnilsValidatorTest.java
@@ -0,0 +1,15 @@
+package ru.ylab.exampleone.chernikov.tasktwo.snils_validator;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 12.03.2023
+ */
+public class SnilsValidatorTest {
+    public static void main(String[] args) {
+        SnilsValidator snilsValidator = new SnilsValidatorImpl();
+        System.out.println(snilsValidator.validate("01468870570"));
+        System.out.println(snilsValidator.validate("90114404441"));
+        System.out.println(snilsValidator.validate("02229742636"));
+    }
+}
