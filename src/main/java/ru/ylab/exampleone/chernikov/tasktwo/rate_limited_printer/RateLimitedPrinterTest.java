225d72982a133a28f1549dc1a8bd70fa52866bd3
OvercomingJunk <overcomingjunk@yandex.ru>
2023-03-12 17:52:16 +0300

    Solved "RateLimitedPrinter" task

---
+++ src/main/java/ru/ylab/exampleone/chernikov/tasktwo/rate_limited_printer/RateLimitedPrinterTest.java
@@ -0,0 +1,15 @@
+package ru.ylab.exampleone.chernikov.tasktwo.rate_limited_printer;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 12.03.2023
+ */
+public class RateLimitedPrinterTest {
+    public static void main(String[] args) {
+        RateLimitedPrinter rateLimitedPrinter = new RateLimitedPrinterImpl(2000);
+        for (int i = 0; i < 1_000_000_000; i++) {
+            rateLimitedPrinter.print(String.valueOf(i));
+        }
+    }
+}
