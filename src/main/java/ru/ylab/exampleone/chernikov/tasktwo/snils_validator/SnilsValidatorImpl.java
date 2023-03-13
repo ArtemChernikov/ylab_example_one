85c6ef309ac61dd9201500b4e90a771ad151cb00
OvercomingJunk <overcomingjunk@yandex.ru>
2023-03-13 19:46:46 +0300

    Simplified SnilsValidatorImpl class

--- src/main/java/ru/ylab/exampleone/chernikov/tasktwo/snils_validator/SnilsValidatorImpl.java
+++ src/main/java/ru/ylab/exampleone/chernikov/tasktwo/snils_validator/SnilsValidatorImpl.java
@@ -8,34 +8,23 @@ package ru.ylab.exampleone.chernikov.tasktwo.snils_validator;
 public class SnilsValidatorImpl implements SnilsValidator {
     @Override
     public boolean validate(String snils) {
-        boolean valid = true;
-        if (snils == null) {
-            valid = false;
-        } else if (snils.length() == 0) {
-            valid = false;
-        } else if (!snils.matches("^[0-9]*$")) {
-            valid = false;
-        } else if (snils.length() != SNILS_LENGTH) {
-            valid = false;
-        } else {
-            int sum = 0;
-            for (int i = 0; i < LENGTH_OF_SNILS_NUMBER; i++) {
-                sum += Character.digit(snils.charAt(i), 10) * (LENGTH_OF_SNILS_NUMBER - i);
-            }
-            int checkControlNumber = 0;
-            if (sum < 100) {
-                checkControlNumber = sum;
-            } else if (sum > 100) {
-                checkControlNumber = sum % 101;
-                if (checkControlNumber == 100) {
-                    checkControlNumber = 0;
-                }
-            }
-            int realControlNumber = Integer.parseInt(snils.substring(LENGTH_OF_SNILS_NUMBER));
-            if (checkControlNumber != realControlNumber) {
-                valid = false;
+        if (snils == null || snils.length() != SNILS_LENGTH || !snils.matches("^[0-9]*$")) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 0; i < LENGTH_OF_SNILS_NUMBER; i++) {
+            sum += Character.digit(snils.charAt(i), 10) * (LENGTH_OF_SNILS_NUMBER - i);
+        }
+        int checkControlNumber = 0;
+        if (sum < 100) {
+            checkControlNumber = sum;
+        } else if (sum > 100) {
+            checkControlNumber = sum % 101;
+            if (checkControlNumber == 100) {
+                checkControlNumber = 0;
             }
         }
-        return valid;
+        int realControlNumber = Integer.parseInt(snils.substring(LENGTH_OF_SNILS_NUMBER));
+        return checkControlNumber == realControlNumber;
     }
 }
