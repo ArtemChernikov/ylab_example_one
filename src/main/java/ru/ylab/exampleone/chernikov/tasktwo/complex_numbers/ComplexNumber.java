28d60f2860c2b93e49ae1c25e4aa4e00be32c2b3
OvercomingJunk <overcomingjunk@yandex.ru>
2023-03-13 19:38:45 +0300

    Changed ComplexNumber class

--- src/main/java/ru/ylab/exampleone/chernikov/tasktwo/complex_numbers/ComplexNumber.java
+++ src/main/java/ru/ylab/exampleone/chernikov/tasktwo/complex_numbers/ComplexNumber.java
@@ -42,6 +42,52 @@ public class ComplexNumber {
         this.imaginaryPart = imaginaryPart;
     }
 
+    /**
+     * Метод используется для сложения комплексного числа, на котором вызывается метод
+     * и комплексного числа, которое задается в параметрах метода
+     *
+     * @param number - комплексное число
+     * @return - возвращает результат суммирования в виде нового комплексного числа
+     */
+    public ComplexNumber sum(ComplexNumber number) {
+        return new ComplexNumber(this.getRealPart() + number.getRealPart(),
+                this.getImaginaryPart() + number.getImaginaryPart());
+    }
+
+    /**
+     * Метод используется для вычитания из комплексного числа, на котором вызвается метод,
+     * комплексного числа, которое задается в параметрах метода
+     *
+     * @param number - комплексное число
+     * @return - возвращает результат вычитания в виде нового комплексного числа
+     */
+    public ComplexNumber subtract(ComplexNumber number) {
+        return new ComplexNumber(this.getRealPart() - number.getRealPart(),
+                this.getImaginaryPart() - number.getImaginaryPart());
+    }
+
+    /**
+     * Метод используется для умножения комплексного числа, на котором вызывается метод
+     * с комплексным числом, которое задается в параметрах метода
+     *
+     * @param number - комплексное число
+     * @return - возвращает результат умножения в виде нового комплексного числа
+     */
+    public ComplexNumber multiply(ComplexNumber number) {
+        return new ComplexNumber(
+                this.getRealPart() * number.getRealPart() - this.getImaginaryPart() * number.getImaginaryPart(),
+                this.getRealPart() * number.getImaginaryPart() + this.getImaginaryPart() * number.getRealPart());
+    }
+
+    /**
+     * Метод используется для вычисления модуля комплексного числа
+     *
+     * @return - возвращает модуль комплексного числа
+     */
+    public double module() {
+        return Math.sqrt(Math.pow(this.getRealPart(), 2) + Math.pow(this.getImaginaryPart(), 2));
+    }
+
     @Override
     public String toString() {
         if (realPart == 0 && imaginaryPart == 0) {
