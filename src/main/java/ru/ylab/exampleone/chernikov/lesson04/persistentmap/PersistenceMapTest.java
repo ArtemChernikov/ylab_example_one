3c05b6f723ea89be1d94928a38d4c108c125a5e3
OvercomingJunk <overcomingjunk@yandex.ru>
2023-03-28 21:09:55 +0300

    Solved "Persistent Map" task

--- src/main/java/ru/ylab/exampleone/chernikov/lesson04/persistentmap/PersistenceMapTest.java
+++ src/main/java/ru/ylab/exampleone/chernikov/lesson04/persistentmap/PersistenceMapTest.java
@@ -6,22 +6,36 @@ import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class PersistenceMapTest {
-  public static void main(String[] args) throws SQLException {
-    DataSource dataSource = initDb();
-    PersistentMap persistentMap = new PersistentMapImpl(dataSource);
-    // Написать код демонстрации работы 
-  }
-  
-  public static DataSource initDb() throws SQLException {
-    String createMapTable = "" 
-                                + "drop table if exists persistent_map; " 
-                                + "CREATE TABLE if not exists persistent_map (\n"
-                                + "   map_name varchar,\n"
-                                + "   KEY varchar,\n"
-                                + "   value varchar\n"
-                                + ");";
-    DataSource dataSource = DbUtil.buildDataSource();
-    DbUtil.applyDdl(createMapTable, dataSource);
-    return dataSource;
-  }
+    public static void main(String[] args) throws SQLException {
+        DataSource dataSource = initDb();
+        PersistentMap persistentMap = new PersistentMapImpl(dataSource);
+        persistentMap.init("map1");
+        persistentMap.put("key1", "value1");
+        System.out.println(persistentMap.containsKey("key1"));
+        persistentMap.remove("key1");
+        System.out.println(persistentMap.containsKey("key1"));
+
+        persistentMap.put("key1", "value1");
+        persistentMap.put("key2", "value2");
+        System.out.println(persistentMap.getKeys());
+
+        System.out.println(persistentMap.get("key2"));
+
+        System.out.println(persistentMap.getKeys());
+        persistentMap.clear();
+        System.out.println(persistentMap.getKeys());
+    }
+
+    public static DataSource initDb() throws SQLException {
+        String createMapTable = ""
+                + "drop table if exists persistent_map; "
+                + "CREATE TABLE if not exists persistent_map (\n"
+                + "   map_name varchar,\n"
+                + "   KEY varchar,\n"
+                + "   value varchar\n"
+                + ");";
+        DataSource dataSource = DbUtil.buildDataSource();
+        DbUtil.applyDdl(createMapTable, dataSource);
+        return dataSource;
+    }
 }
--- src/main/java/ru/ylab/exampleone/chernikov/lesson04/persistentmap/PersistentMapImpl.java
+++ src/main/java/ru/ylab/exampleone/chernikov/lesson04/persistentmap/PersistentMapImpl.java
@@ -1,52 +1,164 @@
 package ru.ylab.exampleone.chernikov.lesson04.persistentmap;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Класс, методы которого надо реализовать 
+ * Класс, методы которого надо реализовать
  */
 public class PersistentMapImpl implements PersistentMap {
-  
-  private DataSource dataSource;
 
-  public PersistentMapImpl(DataSource dataSource) {
-    this.dataSource = dataSource;
-  }
+    /**
+     * Поле для создания соединений с БД
+     */
+    private final DataSource dataSource;
 
-  @Override
-  public void init(String name) {
-    
-  }
+    /**
+     * Поле имя экземпляра map с которым происходит работа
+     */
+    private String name;
 
-  @Override
-  public boolean containsKey(String key) throws SQLException {
-    return false;
-  }
+    public PersistentMapImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
-  @Override
-  public List<String> getKeys() throws SQLException {
-    return null;
-  }
+    /**
+     * Метод используется для инициализации map, с которым будет происходить работа
+     *
+     * @param name - имя map
+     */
+    @Override
+    public void init(String name) {
+        this.name = name;
+    }
 
-  @Override
-  public String get(String key) throws SQLException {
-    return null;
-  }
+    /**
+     * Метод используется для проверки находится ли данный ключ в БД
+     *
+     * @param key - ключ
+     * @return - возвращает true, если ключ существует, false, если иначе
+     * @throws SQLException - может выбросить {@link SQLException}
+     */
+    @Override
+    public boolean containsKey(String key) throws SQLException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT * FROM persistent_map WHERE map_name = ? AND key = ?;"
+             )) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, key);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-  @Override
-  public void remove(String key) throws SQLException {
+    /**
+     * Метод используется для получения всех ключей определенной map из БД
+     *
+     * @return - возвращает список ключей
+     * @throws SQLException - может выбросить {@link SQLException}
+     */
+    @Override
+    public List<String> getKeys() throws SQLException {
+        List<String> keys = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT key FROM persistent_map WHERE map_name = ?;"
+             )) {
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                keys.add(resultSet.getString(1));
+            }
+        }
+        return keys;
+    }
 
-  }
+    /**
+     * Метод используется для получения значения по ключу из БД
+     *
+     * @param key - ключ
+     * @return - возвращает значение по ключу, если ключ сущетсвует, иначе null
+     * @throws SQLException - может выбросить {@link SQLException}
+     */
+    @Override
+    public String get(String key) throws SQLException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT value FROM persistent_map WHERE key = ? AND map_name = ?;"
+             )) {
+            preparedStatement.setString(1, key);
+            preparedStatement.setString(2, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getString(1);
+            }
+        }
+        return null;
+    }
 
-  @Override
-  public void put(String key, String value) throws SQLException {
+    /**
+     * Метод используется для удаления из БД пары ключ-значения по ключу определенной map
+     *
+     * @param key - ключ
+     * @throws SQLException - может выбросить {@link SQLException}
+     */
+    @Override
+    public void remove(String key) throws SQLException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "DELETE FROM persistent_map WHERE key = ? AND map_name = ?;"
+             )) {
+            preparedStatement.setString(1, key);
+            preparedStatement.setString(2, name);
+            preparedStatement.executeUpdate();
+        }
+    }
 
-  }
+    /**
+     * Метод используется для записи новой пары ключ-значение в БД
+     *
+     * @param key   - ключ
+     * @param value - значение
+     * @throws SQLException - может выбросить {@link SQLException}
+     */
+    @Override
+    public void put(String key, String value) throws SQLException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "INSERT INTO persistent_map VALUES(?, ?, ?);"
+             )) {
+            if (containsKey(key)) {
+                remove(key);
+            }
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, key);
+            preparedStatement.setString(3, value);
+            preparedStatement.execute();
+        }
+    }
 
-  @Override
-  public void clear() throws SQLException {
-
-  }
+    /**
+     * Метод используется для удаления всех пар ключ-значений определенной map в БД
+     *
+     * @throws SQLException - может выбросить {@link SQLException}
+     */
+    @Override
+    public void clear() throws SQLException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "DELETE FROM persistent_map WHERE map_name = ?;"
+             )) {
+            preparedStatement.setString(1, name);
+            preparedStatement.executeUpdate();
+        }
+    }
 }
