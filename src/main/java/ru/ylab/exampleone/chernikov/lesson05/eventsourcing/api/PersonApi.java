38a7eff6c4c54216e5bf19395902acffcf25f068
OvercomingJunk <overcomingjunk@yandex.ru>
2023-03-31 22:17:34 +0300

    Added a skeleton of classes

---
+++ src/main/java/ru/ylab/exampleone/chernikov/lesson05/eventsourcing/api/PersonApi.java
@@ -0,0 +1,15 @@
+package ru.ylab.exampleone.chernikov.lesson05.eventsourcing.api;
+
+import io.ylab.intensive.lesson05.eventsourcing.Person;
+
+import java.util.List;
+
+public interface PersonApi {
+  void deletePerson(Long personId);
+
+  void savePerson(Long personId, String firstName, String lastName, String middleName);
+
+  Person findPerson(Long personId);
+
+  List<Person> findAll();
+}
