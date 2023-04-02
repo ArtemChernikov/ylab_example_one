f0ed4dc0bc9427a80c0dafdc950f787112ce58dd
OvercomingJunk <overcomingjunk@yandex.ru>
2023-04-02 14:07:24 +0300

    Solved "Query Extender" task

--- src/main/java/ru/ylab/exampleone/chernikov/lesson05/sqlquerybuilder/Config.java
+++ src/main/java/ru/ylab/exampleone/chernikov/lesson05/sqlquerybuilder/Config.java
@@ -4,21 +4,30 @@ import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import ru.ylab.exampleone.chernikov.lesson05.sqlquerybuilder.connectionwrapper.DatabaseConnectionWrapper;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Configuration
 @ComponentScan("ru.ylab.exampleone.chernikov.lesson05.sqlquerybuilder")
 public class Config {
-  
-  @Bean
-  public DataSource dataSource() {
-    PGSimpleDataSource dataSource = new PGSimpleDataSource();
-    dataSource.setServerName("localhost");
-    dataSource.setUser("postgres");
-    dataSource.setPassword("postgres");
-    dataSource.setDatabaseName("postgres");
-    dataSource.setPortNumber(5432);
-    return dataSource;
-  }
+
+    @Bean
+    public DataSource dataSource() {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setServerName("localhost");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("postgres");
+        dataSource.setDatabaseName("postgres");
+        dataSource.setPortNumber(5432);
+        return dataSource;
+    }
+
+    @Bean(destroyMethod = "destroy")
+    public DatabaseConnectionWrapper databaseConnectionWrapper(DataSource dataSource) throws SQLException {
+        Connection databaseConnection = dataSource.getConnection();
+        return new DatabaseConnectionWrapper(databaseConnection);
+    }
 }
---
+++ src/main/java/ru/ylab/exampleone/chernikov/lesson05/sqlquerybuilder/SQLQueryBuilderImpl.java
@@ -0,0 +1,100 @@
+package ru.ylab.exampleone.chernikov.lesson05.sqlquerybuilder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.ylab.exampleone.chernikov.lesson05.sqlquerybuilder.connectionwrapper.DatabaseConnectionWrapper;
+
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Artem Chernikov
+ * @version 1.0
+ * @since 02.04.2023
+ */
+@Component
+public class SQLQueryBuilderImpl implements SQLQueryBuilder {
+    /**
+     * Поле для работы с БД
+     */
+    private final DatabaseConnectionWrapper databaseConnectionWrapper;
+
+    @Autowired
+    public SQLQueryBuilderImpl(DatabaseConnectionWrapper databaseConnectionWrapper) {
+        this.databaseConnectionWrapper = databaseConnectionWrapper;
+    }
+
+    /**
+     * Метод используется для получения запроса “SELECT <col1>, <col2>, <col3> FROM <tablename>;"
+     * в виде строки из таблицы по ее имени
+     *
+     * @param tableName - имя таблицы
+     * @return - возвращает запрос, если таблица существует, null если нет,
+     * (если таблица существует, но у нее нет столбцов возвращает "Table with no columns")
+     * @throws SQLException - может выбросить {@link SQLException}
+     */
+    @Override
+    public String queryForTable(String tableName) throws SQLException {
+        DatabaseMetaData metaData = databaseConnectionWrapper.getConnection().getMetaData();
+        ResultSet table = metaData.getTables(null, null, tableName, null);
+        if (!table.next()) {
+            table.close();
+            return null;
+        }
+        ResultSet columns = metaData.getColumns(null, null, tableName, null);
+        List<String> columnsList = new ArrayList<>();
+        while (columns.next()) {
+            String columnName = columns.getString("COLUMN_NAME");
+            columnsList.add(columnName);
+        }
+        columns.close();
+        return getQuery(columnsList, tableName);
+    }
+
+    /**
+     * Метод используется для получения списка имен всех существующих таблиц в БД
+     *
+     * @return - возвращает список таблиц
+     * @throws SQLException - может выбросить {@link SQLException}
+     */
+    @Override
+    public List<String> getTables() throws SQLException {
+        DatabaseMetaData metaData = databaseConnectionWrapper.getConnection().getMetaData();
+        ResultSet resultSet = metaData.getTables(null, null, "%", null);
+        List<String> tablesNames = new ArrayList<>();
+        while (resultSet.next()) {
+            tablesNames.add(resultSet.getString("TABLE_NAME"));
+        }
+        resultSet.close();
+        return tablesNames;
+    }
+
+    /**
+     * Метод используется для составления запроса по входным данным:
+     * “SELECT <col1>, <col2>, <col3> FROM <tablename>;"
+     *
+     * @param columns   - список названий столбцов таблицы
+     * @param tableName - имя таблицы
+     * @return - возвращает запрос SELECT, если таблицы со столбцами и "Table with no columns", если иначе
+     */
+    private String getQuery(List<String> columns, String tableName) {
+        if (columns.isEmpty()) {
+            return "Table with no columns";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("SELECT ");
+        for (int i = 0; i < columns.size(); i++) {
+            stringBuilder.append(columns.get(i));
+            if (i < (columns.size() - 1)) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append(" FROM ");
+        stringBuilder.append(tableName);
+        stringBuilder.append(";");
+        return stringBuilder.toString();
+    }
+}
